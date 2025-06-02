3fc1a4ea35bee8b75d84fc56539a6e751a19dffb
johnwgomez <johnwgomez10@gmail.com>
2025-06-01 20:12:55 -0700

    database.ts updated

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,6 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in environment variables.');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
